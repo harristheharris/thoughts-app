b64d015afaff8152cbaff96142b58ff9487c87a8
harristheharris <anthonyharris7777@gmail.com>
2024-06-02 15:51:07 -0400

    exporting our future models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//exporting our models
+console.log(`Export models Thought and ThoughtUser`);
+const Thought = require("./Thought");
+const ThoughtUser = require("./ThoughtUser");
+
+module.exports = { Thought, ThoughtUser}
