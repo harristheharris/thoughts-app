cb98f209e64343c1f0a6162eba07747e6c69f5e4
harristheharris <anthonyharris7777@gmail.com>
2024-06-02 21:01:48 -0400

    added create, various updates, delete for user controller

--- controllers/thoughtUser-controller.js
+++ controllers/thoughtUser-controller.js
@@ -45,7 +45,86 @@ module.exports = {
             return res.status(400).json(err);
         }
     },
-    
+
+    async addUser(req, res) {
+        try {
+            const user = await ThoughtUser.create(req.body)
+            res.json(user)
+
+        } catch (err) {
+            res.status(500).json(err);
+
+        }
+    },
+
+
+    async deleteUser(req, res) {
+        try {
+            const user = await ThoughtUser.findOneAndDelete({ _id: req.params.id })
+                .then((dbUserData) => {
+                    if (!dbUserData) {
+                        return res.status(400).json({ message: "no user with this id" })
+                    }
+                })
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+
+            const user = await ThoughtUser.findOneAndUpdate(
+                { _id: params.id },
+                { $set: req.body },
+                { runValidators: true, new: true });
+
+            if (!user) {
+                return res.status(404).json({ message: 'no user with this id' })
+            }
+
+            res.json(user)
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async addFriend({ params }, res) {
+        try {
+            const user = await ThoughtUser.findByIdAndUpdate(
+                { _id: params.userId },
+                { $addToSet: { friends: params.friendId } },
+                { nre: true, runValidators: true }
+            )
+
+            if (!user) {
+                res.status(404).json({ message: "no user with that id" })
+                return;
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async removeFriend( { params }, res) {
+        try {
+            const user = await ThoughtUser.findByIdAndUpdate(
+                { _id: params.userId },
+                { $pill: { friends: params.friendId } },
+                { new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: "no user with this id" });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
 
 
 
